c0ce2416d7d6576089768ee86ae3ac69893a6ad6
subhojitc33 <subhojitc3@gmail.com>
2022-12-23 14:09:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(
       defaultSrc: ["'self'", "default.example"],
       scriptSrc: ["'self'", "js.example.com"],
       objectSrc: ["'none'"],
-	    frame-ancestors : ["self", "https://*.salesforce.com/", "https://*.force.com/"],
+	    frame-ancestors : ["'self'", "'https://newdev3-dev-ed.my.salesforce.com/'", "https://newdev3-dev-ed.lightning.force.com/'"],
       upgradeInsecureRequests: [],
     },
     reportOnly: false,
